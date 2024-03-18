0bb2a9d7a2e6d2e3b72aac942423fe39b5a377af
Vanshika Rana <aahiknsv@Vanshikas-MacBook-Air.local>
2024-03-19 00:00:26 +0530

    Avo starter dapp

--- app/page.js
+++ app/page.js
@@ -1,113 +1,212 @@
+"use client";
+import { useRef, useState, useEffect } from "react";
+import { ethers } from "ethers";
+import Web3Modal from "web3modal";
+import { IoCopyOutline } from "react-icons/io5";
+import avoForwarderV1ABI from "../constants/avo-forwarder-v1-abi.json";
 import Image from "next/image";
+import logolight from "../public/images/logolight.png"
+import logo from "../public/images/logo.png"
+import Footer from "@/components/Footer";
 
+// Define chain configurations
+const chains = {
+  avocado: {
+    rpcUrl: "https://rpc.avocado.instadapp.io/",
+    forwarderAddress: "0x46978CD477A496028A18c02F07ab7F35EDBa5A54",
+  },
+  polygon: {
+    rpcUrl: "https://polygon-rpc.com/",
+    forwarderAddress: "0x46978CD477A496028A18c02F07ab7F35EDBa5A54",
+  },
+  // Add more chains here as needed
+};
+
+// Main component
 export default function Home() {
+  const [connectedAddress, setConnectedAddress] = useState("");
+  const [avocadoAddress, setAvocadoAddress] = useState("");
+  const [avocadoMultisigAddress, setAvocadoMultisigAddress] = useState("");
+  const [avocadoBalance, setAvocadoBalance] = useState("");
+  const [polygonBalance, setPolygonBalance] = useState("");
+  const [avocadoMultisigBalance, setAvocadoMultisigBalance] = useState("");
+  const web3ModalRef = useRef();
+
+  // Initialize Web3Modal on component mount
+  useEffect(() => {
+    web3ModalRef.current = new Web3Modal({
+      network: "Avocado RPC",
+      providerOptions: {},
+      disableInjectedProvider: false,
+    });
+  }, []);
+
+  // Function to connect wallet
+  const connectWallet = async () => {
+    try {
+      const provider = await web3ModalRef.current.connect();
+      if (provider) {
+        const web3Provider = new ethers.providers.Web3Provider(provider);
+        const signer = web3Provider.getSigner();
+        const address = await signer.getAddress();
+        setConnectedAddress(address);
+        await createAvoWallet(address);
+      }
+    } catch (err) {
+      console.error("Error connecting wallet:", err);
+    }
+  };
+
+  // Function to create Avocado wallet
+  const createAvoWallet = async (address) => {
+    try {
+      const provider = new ethers.providers.JsonRpcProvider(
+        chains.polygon.rpcUrl
+      );
+      const forwarder = new ethers.Contract(
+        chains.polygon.forwarderAddress,
+        avoForwarderV1ABI,
+        provider
+      );
+
+      const index = 0;
+      const avocadoAddress = await forwarder.computeAvocado(address, index);
+      setAvocadoAddress(avocadoAddress);
+
+       /**
+      The index for multisig starts with 1, 
+      a user can have more than one multisig wallet. 
+      Hence, the index will increase accordingly
+       **/
+      const multisigIndex = 1; 
+     
+      const avocadoMultisigAddress = await forwarder.computeAvocado(address, multisigIndex);
+      setAvocadoMultisigAddress(avocadoMultisigAddress);
+
+      const avocadoBalance = await fetchAvocadoBalance(avocadoAddress);
+      setAvocadoBalance(avocadoBalance);
+
+      const polygonBalance = await fetchPolygonBalance(avocadoAddress);
+      setPolygonBalance(polygonBalance);
+
+      const multisigBalance = await fetchAvocadoBalance(avocadoMultisigAddress);
+      setAvocadoMultisigBalance(multisigBalance);
+
+
+    } catch (err) {
+      console.error("Error creating Avocado wallet:", err);
+    }
+  };
+
+  // Function to fetch Polygon balance
+  const fetchPolygonBalance = async (address) => {
+    try {
+      const polygonProvider = new ethers.providers.JsonRpcProvider(chains.polygon.rpcUrl);
+      const balance = await polygonProvider.getBalance(address);
+      return ethers.utils.formatEther(balance);
+    } catch (err) {
+      console.error("Error fetching Polygon balance:", err);
+      return "";
+    }
+  };
+
+  // Function to fetch Avocado balance
+  const fetchAvocadoBalance = async (address) => {
+    try {
+      const avocadoProvider = new ethers.providers.JsonRpcProvider(chains.avocado.rpcUrl);
+      const balance = await avocadoProvider.getBalance(address);
+      return ethers.utils.formatEther(balance);
+    } catch (err) {
+      console.error("Error fetching Avocado balance:", err);
+      return "";
+    }
+  };
+
+  // Function to copy text to clipboard
+  const copyToClipboard = (text) => {
+    navigator.clipboard.writeText(text);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col items-center justify-between">
+    <section className="flex flex-col h-[80vh] items-center justify-center px-4 sm:px-6 lg:px-8">
+      <Image src={logolight} width={200} height={500} />
+      <span className="text-5xl sm:text-7xl font-bold mb-8 mt-2 text-center">Starter Dapp</span>
+      {!connectedAddress && (
+        <button
+          onClick={connectWallet}
+          className="bg-emerald-950 text-white font-semibold px-6 py-2 rounded-lg flex items-center justify-center gap-2 transition-transform duration-500 transform hover:scale-95"
+        >
+          <Image src={logo} width={20} height={20} alt="Logo" />
+          <span>Connect your wallet</span>
+        </button>
+      )}
+      {connectedAddress && (
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-8 w-full">
+          <div className="border border-gray-300 rounded-lg p-4 flex flex-col items-center">
+            <h2 className="font-bold mb-4">Connected EOA Address</h2>
+            <div className="flex items-center">
+              <p className="mr-2" title={connectedAddress}>
+                {connectedAddress.slice(0, 6) +
+                  "..." +
+                  connectedAddress.slice(-4)}
+              </p>
+              <button
+                onClick={() => copyToClipboard(connectedAddress)}
+                className="text-green-600 px-4 py-1 rounded-full hover:text-green-700 hover:scale-95"
+              >
+                <IoCopyOutline />
+              </button>
+            </div>
+          </div>
+          <div className="border border-gray-300 rounded-lg p-4 flex flex-col items-center">
+            <h2 className="font-bold mb-4">Avocado Address</h2>
+            <div className="flex items-center">
+              <p className="mr-2" title={avocadoAddress}>
+                {avocadoAddress.slice(0, 6) +
+                  "..." +
+                  avocadoAddress.slice(-4)}
+              </p>
+              <button
+                onClick={() => copyToClipboard(avocadoAddress)}
+                className="text-green-600 px-4 py-1 rounded-full hover:text-green-700 hover:scale-95"
+              >
+                <IoCopyOutline />
+              </button>
+            </div>
+          </div>
+          <div className="border border-gray-300 rounded-lg p-4 flex flex-col items-center">
+            <h2 className="font-bold mb-4">Avocado Multisig Address #1</h2>
+            <div className="flex items-center">
+              <p className="mr-2" title={avocadoMultisigAddress}>
+                {avocadoMultisigAddress.slice(0, 6) +
+                  "..." +
+                  avocadoMultisigAddress.slice(-4)}
+              </p>
+              <button
+                onClick={() => copyToClipboard(avocadoMultisigAddress)}
+                className="text-green-600 px-4 py-1 rounded-full hover:text-green-700 hover:scale-95"
+              >
+                <IoCopyOutline />
+              </button>
+            </div>
+          </div>
+          <div className="border border-gray-300 rounded-lg p-4 flex flex-col items-center">
+            <h2 className="font-bold mb-4">Avocado GAS TANK Balance</h2>
+            <p>{avocadoBalance} USDC</p>
+          </div>
+          <div className="border border-gray-300 rounded-lg p-4 flex flex-col items-center">
+            <h2 className="font-bold mb-4">Polygon Matic Balance</h2>
+            <p>{polygonBalance} MATIC</p>
+          </div>
+          <div className="border border-gray-300 rounded-lg p-4 flex flex-col items-center">
+            <h2 className="font-bold mb-4">Multisig #1 Gas Tank Balance</h2>
+            <p>{avocadoMultisigBalance} USDC</p>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      )}
+</section>
+<Footer/>
     </main>
   );
 }
---
+++ components/Footer.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import logo from "../public/images/logo.png"
+import { FaDiscord } from 'react-icons/fa';
+import { FaXTwitter } from "react-icons/fa6";
+import Image from "next/image";
+
+const Footer = () => {
+  return (
+   
+<footer className="w-full bg-gray-100 py-6 font-bold">
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="flex flex-col md:flex-row justify-between items-center justify-center">
+        {/* First column */}
+        <div className="mb-4 md:mb-0">
+          <Image src={logo} alt="Logo" width={100} height={100} />
+        </div>
+        {/* Second column */}
+        <div className="mb-4 md:mb-0">
+          <ul className="flex space-x-4">
+            <li>
+              <a href="https://docs.avocado.instadapp.io/" target='_blank' className="text-gray-600 hover:text-gray-900">
+                Docs
+              </a>
+            </li>
+            <li>
+              <a href="https://docs.avocado.instadapp.io/integrate/avocado-network" target='_blank' className="text-gray-600 hover:text-gray-900">
+                Avocado RPC
+              </a>
+            </li>
+            
+          </ul>
+        </div>
+        {/* Third column */}
+        <div>
+  <ul className="flex flex-col items-right">
+    <li>
+      <a href="https://twitter.com/avowallet" target='_blank' className="text-gray-600 hover:text-gray-900">
+        <FaXTwitter className="inline-block mr-2" /> X/Twitter
+      </a>
+    </li>
+    <li>
+      <a href="https://discord.gg/83vvrnY" target='_blank' className="text-gray-600 hover:text-gray-900">
+        <FaDiscord className="inline-block mr-2" /> Discord
+      </a>
+    </li>
+  </ul>
+</div>
+      </div>
+    </div>
+  </footer>
+  )
+}
+
+export default Footer
